062c3063672ca54ae2ffc80c3e31131eeb4545e8
shimul-iut <31004910+shimul-iut@users.noreply.github.com>
2020-03-11 22:40:14 +0600

    Final commit before submission

--- scraper.py
+++ scraper.py
@@ -5,12 +5,21 @@ import requests
 import argparse
 import os
 
+#Retrived URL for the latest questions posted in past week
+def latest_post_urls(a):
+    return 'https://stackoverflow.com/search?tab=newest&q=%5b'+a+'%5d%20is%3aquestion%20created%3a7d..'
+#Retrived URL for the most voted questions posted in past week
+def most_voted_posts_url(a):
+    return 'https://stackoverflow.com/search?q=['+a+']+is%3Aquestion+created%3A7d..&tab=votes'
+
+#Initial parsing of the search result page using BeautifulSoup
 def find_body(url):
 
     parse = requests.get(url)
     soup = bs(parse.content, 'html.parser')
     return soup.find('body')
 
+#Scrapping some basic data regarding the questions
 def find_details(url):
 
     body = find_body(url)
@@ -29,6 +38,7 @@ def find_details(url):
     merged_list = tuple(zip(titles, timestamps, find_votecounts))
     return merged_list
 
+#Scrapping hyperlinks of the questions
 def find_links(url):
 
     body = find_body(url)
@@ -39,24 +49,28 @@ def find_links(url):
     links =  [x.replace("\r\n","") for x in links]
     return links
 
+#Opening the chosen title's associated link inside the bash console performing shell command using os.system()
 def get_data_from_the_title(value, a):
     if "LP" in value:
-        links = find_links('https://stackoverflow.com/search?tab=newest&q=%5b'+a+'%5d%20is%3aquestion%20created%3a7d..')[:10]
+        links = find_links(latest_post_urls(a))[:10]
         url = links[int(value[2:]) - 1]
     elif "MV" in value:
-        links = find_links('https://stackoverflow.com/search?q=['+a+']+is%3Aquestion+created%3A7d..&tab=votes')[:10]
+        links = find_links(most_voted_posts_url(a))[:10]
         url = links[int(value[2:]) - 1]
     else:
         print('wrong input')
+        return
+    print("The stackoverflow URL for the question is : \n")
     print(url)
     os.system("xdg-open " + url)
 
+#Showing the posts inside a Tabular view
 def print_tables(a):
 
     table_data1 = [
         ['#', 'Latest 10 '+ a + ' questions posted past week', 'Created at', 'Votes']
     ]
-    latest_data  = find_details('https://stackoverflow.com/search?tab=newest&q=%5b'+a+'%5d%20is%3aquestion%20created%3a7d..')[:10]
+    latest_data  = find_details(latest_post_urls(a))[:10]
 
     count = 0
     for i in range(len(latest_data)):
@@ -71,7 +85,7 @@ def print_tables(a):
         ['#' , 'Most Voted 10 '+ a + ' questions posted past week', 'Created at', 'Votes']
     ]
 
-    latest_voted =  find_details('https://stackoverflow.com/search?q=['+a+']+is%3Aquestion+created%3A7d..&tab=votes')[:10]
+    latest_voted =  find_details(most_voted_posts_url(a))[:10]
 
     count = 0
     for i in range(len(latest_voted)):
@@ -82,32 +96,11 @@ def print_tables(a):
     table.inner_row_border = True
     print (table.table)
 
-def print_links_latest(a, b):
-
-    table_data = [
-        ['Link for the selected question latest from past week']
-    ]
-    latest_data  = find_links('https://stackoverflow.com/search?tab=newest&q=%5b'+a+'%5d%20is%3aquestion%20created%3a7d..')[:10]
-
-    table_data.append([latest_data])
-
-    table = AsciiTable(table_data)
-    table.inner_row_border = True
-    print (table.table)
-
-def find_details_temp(url):
-
-    find_timestamps = find_body(url).find_all('span', class_='relativetime')
-    timestamps = [i.text for i in find_timestamps]
-    timestamps = [x.replace("\r\n","").strip() for x in timestamps]
-
-    print(timestamps)
-
+#main function
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='This is a simple Stackoverflow scarper!')
-    #parser.add_argument("--searchfor", default="android", type=str)
     parser.add_argument("--searchfor", type=str, default=False)
     args = parser.parse_args()
     searchfor = args.searchfor
@@ -116,5 +109,6 @@ if __name__ == "__main__":
         print_tables(searchfor)
     else :
         print('no paramenters provided')
-    value = raw_input("Please enter a string:\n")
+    print('\n')
+    value = raw_input("Please enter the ID of the question you want to view (i.e : MV8):\n")
     get_data_from_the_title(value, searchfor)
